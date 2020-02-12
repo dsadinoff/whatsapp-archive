4e39b0827037b394556e80bae654ceb4d24a9bc3
Danny Sadinoff <danny@sadinoff.com>
2020-02-12 09:08:01 +0000

    guess the group name

--- whatsapp_archive.py
+++ whatsapp_archive.py
@@ -19,26 +19,34 @@ import unicodedata
 
 # Format of the standard WhatsApp export line. This is likely to change in the
 # future and so this application will need to be updated.
-DATE_RE = '(?P<date>[\d/-]+)'
+
+
+
+DATE_RE = r'(?P<date>[\d/-]+)'
 LRM = r'\u200e'
 LRM_OPT = LRM + '?'
-TIME_RE = '(?P<time>[\d:]+( [AP]M)?)'
-DATETIME_RE = '\[?' + DATE_RE + ',? ' + TIME_RE + '\]?'
-SEPARATOR_RE = '( - |: | )'
-NAME_RE = '(?P<name>[^:]+)'
+TIME_RE = r'(?P<time>[\d:]+( [AP]M)?)'
+DATETIME_RE = r'\[?' + DATE_RE + ',? ' + TIME_RE + '\]?'
+SEPARATOR_RE = r'( - |: | )'
+NAME_RE = r'(?P<name>[^:]+)'
 WHATSAPP_RE = (
     LRM_OPT
     + DATETIME_RE 
     + SEPARATOR_RE 
     +NAME_RE 
-    + ': ' 
+    + r': ' 
     + LRM_OPT 
-    + '(?P<body>.*$)')
+    + r'(?P<body>.*$)')
 
 FIRSTLINE_RE = (DATETIME_RE +
                SEPARATOR_RE +
-               '(?P<body>.*$)')
+               r'(?P<body>.*$)')
 
+
+GROUP_RE_1 = r'created group “(?P<groupName>.+)”'
+GROUP_RE_2 =  (DATETIME_RE +
+               SEPARATOR_RE +
+               r'(?P<groupName>.*?):.*Messages to this group are now secured')
 
 
 class Error(Exception):
@@ -59,13 +67,21 @@ def getColor(userName):
     colorLUT[userName] = colorStr;
     return colorStr;
    
-def ParseLine(line):
+def ParseLine(line, groupName):
     """Parses a single line of WhatsApp export file."""
     if(0==  len(line)):
-        return None;
+        return (None, groupName);
 
     struct = None
     addLinks = True
+
+
+    m = re.match(GROUP_RE_1, line, re.DOTALL)
+    if m:
+        groupName = m.group('groupName')
+    m = re.match(GROUP_RE_2, line, re.DOTALL)
+    if m:
+        groupName = m.group('groupName')
 
     m = re.match(WHATSAPP_RE, line, re.DOTALL)
     if m:
@@ -94,7 +110,7 @@ def ParseLine(line):
     if struct is None:
         logging.warning("parse fail for line: ")
         logging.warning(line.encode("utf-8"));
-        return None
+        return (None, groupName)
 
     # guess directionality of body by sampling first char.  Arguably could be more clever.
     dir="ltr"
@@ -105,7 +121,7 @@ def ParseLine(line):
     
     if addLinks:
         struct['body'] = massageBody(struct['body'])
-    return struct
+    return (struct, groupName)
 
 
 ATTACHMENT_IMG_RE = r'&lt;attached: (?P<filename>.+\.(jpe?g|png|gif|webp))&gt;'
@@ -118,30 +134,31 @@ def massageBody(body):
     body = re.sub(r'>',"&gt;", body)
     body = re.sub(r'(?P<link>https?://.[^ \n<]+)', r'<a href="\g<link>">\g<link></a>', body)
     if  re.search(ATTACHMENT_IMG_RE, body, re.IGNORECASE):
-        body =  re.sub(ATTACHMENT_IMG_RE, r'<a href="\g<filename>"><img src="\g<filename>"></a>', body, flags=re.IGNORECASE)
+        body =  re.sub(ATTACHMENT_IMG_RE, r'<a href="\g<filename>"><img loading=lazy src="\g<filename>"></a>', body, flags=re.IGNORECASE)
     elif  re.search(ATTACHMENT_MOV_RE, body, re.IGNORECASE):
-        body =  re.sub(ATTACHMENT_MOV_RE, r'<a href="\g<filename>"><div class="container"><video src="\g<filename>"></video><div class="centered">PLAY</div></div></a>', body, flags=re.IGNORECASE)
+        body =  re.sub(ATTACHMENT_MOV_RE, r'<a href="\g<filename>"><div class="container"><video loading=lazy src="\g<filename>"></video><div class="centered">PLAY</div></div></a>', body, flags=re.IGNORECASE)
     elif  re.search(ATTACHMENT_AUDIO_RE, body, re.IGNORECASE):
-        body =  re.sub(ATTACHMENT_AUDIO_RE, r'<a href="\g<filename>"><audio controls><source src="\g<filename>" /></audio></a>', body, flags=re.IGNORECASE)
+        body =  re.sub(ATTACHMENT_AUDIO_RE, r'<a href="\g<filename>"><audio loading=lazy controls><source src="\g<filename>" /></audio></a>', body, flags=re.IGNORECASE)
     fixed = re.sub(r'\n','<br>',body)
     return fixed
     
-def IdentifyMessages(lines):
+def IdentifyMessages(lines, fileName):
     """Input text is split by \rs.
     """
     messages = []
     msg_date = None
     msg_user = None
     msg_body = None
+    groupName = fileName
     for line in lines:
-        message = ParseLine(line)
+        message, groupName = ParseLine(line, groupName)
         if  message is None:
             continue;
         messages.append( message )
-    return messages
+    return messages, groupName;
 
 
-def TemplateData(messages, input_filename):
+def TemplateData(messages, input_filename, groupName):
     """Create a struct suitable for procesing in a template.
     Returns:
         A dictionary of values.
@@ -151,6 +168,7 @@ def TemplateData(messages, input_filename):
     # for user, msgs_of_user in itertools.groupby(messages, lambda x: x[1]):
     #     by_user.append((user, list(msgs_of_user)))
     return dict(messages=messages,
+                groupName = groupName,
                 input_basename=file_basename,
                 input_full_path=input_filename)
 
@@ -159,7 +177,7 @@ def FormatHTML(data):
     tmpl = """<!DOCTYPE html>
     <html>
     <head>
-        <title>WhatsApp archive {{ input_basename }}</title>
+        <title>WhatsApp archive {{ groupName }}</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
@@ -193,15 +211,16 @@ body {
     margin:0;
 }
 ol.message-list {
-    background-color:#e5ddd5;
+    # background-color:#e5ddd5;
     list-style-type: none;
     list-style-position: inside;
     margin: 0;
     padding: 0;
 }
 .message-list li{
+    opacity: 1;
     margin:2px 5px;
-    max-width: 65%;
+    max-width: 33rem;
 }
 .message-list .message{
     padding: 6px 7px 8px 9px;
@@ -259,7 +278,21 @@ header{
     z-index:1000;
 }
 
+.message-list-container-overlay{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-repeat:repeat;
+    top:0;
+    background-image: url(https://web.whatsapp.com/img/bg-chat-tile_9e8a2898faedb7db9bf5638405cf81ae.png);
+    opacity: 0.06;
+    pointer-events: none;
+    z-index:-1;
+}
+
 .message-list-container{
+    background-color: #e5ddd5;
+    background-repeat:repeat;
     flex: 1 1 0;
     order : 2 ;
     position: relative;
@@ -277,11 +310,12 @@ header img.group-image{
     <body>
 <div class="main"> 
    <header>
-	<a href="group-image.jpeg"><img  class='group-image' src="group-image.jpeg"></a>
-        <h1>{{ input_basename }}</h1>
+	<a href="pp.jpg"><img  class='group-image' src="pp.jpg"></a>
+        <h1>{{ groupName }}</h1>
 
    </header>
    <div class="message-list-container">
+    <div class="message-list-container-overlay">&nbsp;</div>
         <ol class="message-list">
         {% for message in messages %}
             <li >
@@ -319,8 +353,12 @@ def main():
     with open(args.input_file, 'rb') as fd:
         body = fd.read()
         lines = [ str.decode('utf-8-sig')  for str in body.split( b'\r\n')]
-        messages = IdentifyMessages(lines)
-    template_data = TemplateData(messages, args.input_file)
+
+        # maybe in the future, insist on the zip file, and do the binary unpacking ourselves...
+        if len(lines) == 1:
+            logging.warning("Only a single line found.   Be sure to unpack the zip file in binary mode")
+        (messages,groupName) = IdentifyMessages(lines, fileName=args.input_file)
+    template_data = TemplateData(messages, args.input_file, groupName)
     HTML = FormatHTML(template_data)
     with open(args.output_file, 'w', encoding='utf-8') as fd:
         fd.write(HTML)
